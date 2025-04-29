07b52e61a5c46d173e800652fd72fc0723f36706
warsh <warsh@bu.edu>
2025-04-29 12:59:02 -0400

    firstStuff

---
+++ instrumentConnect.py
@@ -0,0 +1,433 @@
+"""
+This module contains utilities for controlling Jobin-Yvon
+"SPEX" series monochromators over RS232 serial.
+
+Classes defined:
+ * :class:`spex750m` -- implements standard Jobin-Yvon serial commands for simple spectrometers like the 750M.
+ * :class:`triax320` -- extends the :mod:`spex750m` class with the more sophisticated commands used by the triax.
+
+Tutorial
+++++++++
+
+both :class:`spex750m` and :class:`triax320` behave roughly the same
+way. In this tutorial we use the 750M for example. Instantiate like:
+
+>>> beast = spex750m()
+
+This opens a connection with the larger of the two spectrometers (the
+SPEX 750M.)
+
+Classes are configured to look for the spectrometer at the serial
+port where they are normally plugged in. To specify a different serial
+port, just pass it as an optional parameter when instantiating.
+For example, the following should be equivalent:
+
+>>> beast = spex750m(addr=1)
+>>> beast = spex750m(addr="COM2")   # on windows
+>>> beast = spex750m(addr="/dev/ttyS1")   # on linux
+
+It's a good idea to check that the spectrometer has been
+calibrated. This is unneccessary for the Triax, but on the 750M, we
+should check it. Having instantiated it as ``beast``, run
+
+>>> beast.wl
+790.
+
+This queries the current wavelength in nm, which ought to match
+the value displayed in the window. If it doesn't, run
+
+>>> beast.calibrate(800)
+
+This will re-set the calibration to 800nm:
+
+>>> beast.wl
+800.
+
+You can control the spectrometer using the methods documented below.
+For example, this performs a wavelength scan:
+
+>>> beast = spex750m('/dev/ttyUSB0')
+>>> beast.calibrate(800)
+>>> beast.set_wavelength(750)
+>>> for i in range(200):
+...    beast.rel_move(0.5)
+...    while beast.is_busy():
+...        sleep(0.1)
+...    result = measure_something()
+...    print beast.get_wavelength(), result
+
+API documentation
++++++++++++++++++
+
+"""
+
+from time import sleep
+from util import Serial, InstrumentError
+
+class spex750m(object):
+    """
+    A class implementing standard Jobin-Yvon serial commands
+    for simple spectrometers like the SPEX 750M.
+
+    By default, this class is configured to look for the 750M
+    plugged into ``/dev/ttyUSB0`` (the first port on the USB-serial
+    adapter, when run under linux).
+
+    """
+
+    def __init__(self, addr=None):
+        """
+        Connect to the spectrometer and initialize its hardware.
+
+        :param addr: the serial interface where the
+                     spectrometer is found.
+                     Default: ``/dev/ttyUSB0``
+
+        """
+
+        if addr is None:
+            addr = self._default_location
+        self.bus = Serial(addr, timeout=10, baudrate=19200)
+
+        try:
+            self.init_hardware()    # try to initialize hardware.
+        except InstrumentError as err:
+            print("%s, rebooting once..." % err)
+            self.reboot()           # in case of trouble, reboot
+            self.init_hardware()    # and try again.
+
+    # 750M specific data
+    _steps_per_nm = 4000
+    _default_location = '/dev/ttyUSB0'
+
+    def __str__(self):
+        return 'Spex 750M'
+
+    def boot_status_check(self):
+        """Check the boot status of the controller.
+
+        - \* : Just Autobauded
+        - B : Boot Acknowledged
+        - F : Just Flashed
+        """
+        self.bus.readall()
+        resp = self.bus.ask(' ')    # send autobaud character
+        if len(resp) == 0:
+            raise InstrumentError("750M did not give a boot status")
+        elif resp[0] in ("*", "F", "B"):
+            return resp[0]
+        else:
+            raise InstrumentError("Unknown 750M boot status: %s" % resp)
+
+    def reboot(self):
+        """Reboot the controller if it's not responding"""
+        return self.bus.ask("\xDE")
+
+    def _hi_iq(self):
+        """Send the HI IQ character to the controller.
+        (Duplicates functionality of F7-247.vi.)
+        Returns True if it's ok to flash the controller
+        afterward.
+        """
+        self.bus.readall()
+        resp = self.bus.ask("\xF7")
+        if resp[0] == "=":
+            return True
+        else:
+            raise InstrumentError("750M HI IQ command failed")
+
+    def _flash(self):
+        """Flash the controller by sending the O2000<null>
+        string to it.
+        """
+        return self.bus.ask("O2000\x00")
+
+    def init_hardware(self):
+        """Initialize 750M hardware. I don't know why this
+        works, I just copied Yan's LabView routine.
+        """
+        status = self.boot_status_check()
+        if status == "*":
+            self._hi_iq()    # * -> B
+            self._flash()    # B -> F
+        status = self.boot_status_check()
+        if status == "F":
+            return True
+        else:
+            raise InstrumentError("750M hardware init failed.")
+
+    def wait_for_ok(self, expected_bytes=1):
+        """
+        Wait indefinitely for the 'o' status byte.
+
+        This function waits until a certain number of bytes
+        (usually just one) are present on the bus. When
+        that data arrives, the first bye is read
+        and checked to make sure it's the "okay" status.
+
+        """
+        while self.bus.inWaiting() < expected_bytes:
+            # as long as the buffer is empty, hang out
+            sleep(0.050)
+        # read the status byte when it arrives
+        resp = self.bus.read()
+        if resp != 'o':
+            raise InstrumentError("750M operation failed: %s" % resp)
+
+    def calibrate(self, wl_value):
+        """Read the current wavelength from the window
+        and pass it to this method (units of nm) to recalibrate
+        the 750M.
+        """
+        self.bus.readall()
+        cmd = "G0,%d\r" % (wl_value * self._steps_per_nm)
+        self.bus.write(cmd)
+        self.wait_for_ok()
+
+    _busyCodes = {"q": True,
+                 "z": False }
+
+    def is_busy(self):
+        """Check if the motors are busy. """
+        self.bus.write("E")
+        self.wait_for_ok()
+        resp = self.bus.readall()
+        return self._busyCodes[resp]
+
+    def rel_move(self, distance_to_move):
+        """Move the grating by the given
+        number of nanometers."""
+        self.bus.readall()
+        cmd = "F0,%d\r" % (distance_to_move * self._steps_per_nm)
+        self.bus.write(cmd)
+        self.wait_for_ok()
+        while self.is_busy():
+            # wait for the motors to rest
+            sleep(0.050)
+
+    def get_wavelength(self):
+        """Query the current wavelength """
+        self.bus.readall()
+        self.bus.write("HO\r")
+        self.wait_for_ok()
+        resp = self.bus.readall(term_chars='\r')
+        # convert to wavelength units
+        wl = int(resp) / float(self._steps_per_nm)
+        return wl
+
+    def set_wavelength(self, wl):
+        """Move to the wavelength value specified.
+        contingent on proper calibration, of course.
+        """
+        if wl < 0 or wl > 1500:
+            raise InstrumentError("Out of Range")
+        distance_to_move = wl - self.get_wavelength()
+        self.rel_move(distance_to_move)
+
+    wavelength = property(get_wavelength, set_wavelength)
+
+    # abbreviate 'wavelength' to 'wl'
+    get_wl = get_wavelength
+    set_wl = set_wavelength
+    wl = wavelength
+
+
+# the triax series monochromators add some features
+# define a new class which inherits from spex750m
+
+class triax320(spex750m):
+    """
+    A class implementing Jobin-Yvon serial commands
+    for more advanced spectrometers like the TRIAX 320.
+
+    Instantiate as you would a spex750m. If you don't
+    specify a serial port, this class will assume the
+    spectrometer is attached to ``/dev/ttyUSB1``, the second
+    port on the USB-serial converter.
+
+    Unlike the 750M, the Triax
+        - has motorized entrance and exit slits
+        - zeroes its grating on power-up.
+
+    To perform a motor init on the triax, call :meth:`motor_init`.
+
+
+    """
+
+    # different default location.
+    # hopefully the __init__ function will pick this up.
+    _default_location = '/dev/ttyUSB1'
+
+    def __str__(self):
+        return 'Triax 320'
+
+    # the Triax series isn't designed for the steps-per-nm
+    # system (although these commands work).
+    # instead, it works in actual wavelength values.
+    # the following functions shadow the ones defined
+    # above, to prefer the extended 'Z' functions.
+
+    def calibrate(self, wl):
+        """Set wavelength of Triax without moving motor """
+        self.bus.readall()
+        self.bus.write("Z60,1,"+str(wl)+"\r")
+        self.wait_for_ok()
+
+    def get_wavelength(self):
+        """Query the current wavelength """
+        self.bus.readall()
+        self.bus.write("Z62,1\r")
+        self.wait_for_ok(expected_bytes=8)
+        resp = self.bus.readall()
+        return float(resp)
+
+    def set_wavelength(self, wl):
+        """Move to a new wavelength """
+        self.bus.readall()
+        self.bus.write("Z61,1,"+str(wl)+"\r")
+        self.wait_for_ok()
+        while self.is_busy():
+            # wait for the motors to rest
+            sleep(0.050)
+
+    wavelength = property(get_wavelength, set_wavelength)
+
+    # abbreviate 'wavelength' to 'wl'
+    get_wl = get_wavelength
+    set_wl = set_wavelength
+    wl = wavelength
+
+    # general utilities for motorized slits.
+
+    def _move_slit_relative(self, slit_number, amount):
+        """
+        Move a slit motor relatively.
+
+        For example, the following moves slit 0
+        by 5 steps:
+
+        >>> spec._move_slit_relative(0,5)
+
+        """
+        self.bus.readall()
+        self.bus.write("k0,%d,%d\r" % (slit_number, amount))
+        self.wait_for_ok()
+        while self.is_busy():
+            # wait for the motors to rest
+            sleep(0.050)
+
+    def _get_slit_position(self, slit_number):
+        """
+        Read the current absolute position of a slit.
+
+        For example, read the absolute position of slit 0:
+
+        >>> spec._get_slit_position(0)
+        5
+
+        """
+        self.bus.readall()
+        self.bus.write("j0,%d\r" % slit_number)
+        self.wait_for_ok()
+        resp = self.bus.readall()
+        return float(resp)
+
+    def _set_slit_position(self, slit_number, position):
+        """
+        Move a slit motor to a given absolute position.
+
+        Zeroes things out ahead of time for backslash correction.
+
+        """
+        start_position = self._get_slit_position(slit_number)
+        self._move_slit_relative(slit_number, 0 - start_position)
+        self._move_slit_relative(slit_number, position)
+
+    # parameters specific to the triax 320.
+    # entrance slit: 0
+    # exit slit: 2
+
+    entr_slit = property(
+        lambda self: self._get_slit_position(0),
+        lambda self, val: self._set_slit_position(0, val)
+    )
+    """
+    The entrance slit setting.
+
+    >>> beast.entr_slit
+    20.
+    >>> beast.entr_slit = 30
+    >>> beast.entr_slit
+    30.
+
+    """
+
+    exit_slit = property(
+        lambda self: self._get_slit_position(2),
+        lambda self, val: self._set_slit_position(2, val)
+    )
+    """
+    The exit slit setting.
+
+    >>> beast.exit_slit
+    20.
+    >>> beast.exit_slit = 30
+    >>> beast.exit_slit
+    30.
+
+    """
+
+    @property
+    def slits(self):
+        """
+        Return the entrance and exit slit settings together.
+
+        >>> beast.slits
+        (30., 30.)
+
+        Indicates that both slits are at 30. They can also be set
+        together:
+
+        >>> beast.slits = (20,30)
+        >>> beast.slits
+        (20., 30.)
+
+        sets the entrance slit to 20. There is a shortcut for setting
+        the slits to equal values:
+
+        >>> beast.slits = 20
+        >>> beast.slits
+        (20., 20.)
+
+        """
+        return (self.entr_slit, self.exit_slit)
+
+    @slits.setter
+    def slits(self, newvals):
+        if hasattr(newvals, '__iter__'):
+            # if a list or a tuple is given,
+            # adjust entrance and exit slits correspondingly
+            self.entr_slit, self.exit_slit = newvals
+        else:
+            # if the given value is just one number,
+            # set both slits to that value
+            self.entr_slit, self.exit_slit = (newvals, newvals)
+
+    # does the triax 320 autocalibrate?
+    # whatever, here's the code for that.
+
+    def motor_init(self):
+        """
+        Move all motors to their power-up (autocal) positions.
+
+        This zeroes the wavelength grating and
+        both entrance and exit slits.
+        Don't forget to reopen the slits after calling this.
+
+        """
+        self.bus.write("A")
+        self.wait_for_ok()
+        # at this point we could call "i0,0,0\r"
+        # and "i0,2,0" to zero the slits
+        # (yan does that with labview)
+        # but I think this is implied by "A"
---
+++ util.py
@@ -0,0 +1,469 @@
+"""This file provides useful utilities for the wanglib package."""
+
+from time import sleep, time, ctime
+from numpy import array
+from numpy import exp, sqrt, pi
+import numpy
+import logging
+
+class InstrumentError(Exception):
+    """Raise this when talking to instruments fails."""
+    pass
+
+try:
+    import serial
+except ImportError:
+    # PySerial is not installed
+    serial = None
+
+def show_newlines(string):
+    """
+    replace CR+LF with the words "CR" and "LF".
+    useful for debugging.
+
+    """
+    return string.replace(b'\r', b'<CR>').replace(b'\n', b'<LF>')
+
+if serial:
+    class Serial(serial.Serial):
+        """
+        Extension of PySerial_'s :class:`serial.Serial` class that
+        implements a few extra features:
+
+        .. _PySerial: http://pyserial.sourceforge.net/
+
+            - an :meth:`ask` method
+            - a :meth:`readall` method
+            - auto-appended termination characters
+            - in/out logging.
+        
+        To log whatever's written or read to a serial port,
+        pass a filename into the ``log`` kwarg:
+
+        >>> port = Serial('/dev/ttyS0', log='wtf.log')
+
+        To automatically append a newline to each command, specify
+        ``term_chars``:
+
+        >>> port.term_chars = '/n'
+
+        This can also be supplied as a keyword argument.
+
+        """
+
+        def __init__(self, *args, **kwargs):
+            # make an event logger
+            self.logger = logging.getLogger('wanglib.util.Serial')
+            # take 'log' kwarg.
+            self.logfile = kwargs.pop('log', False)
+            if self.logfile:
+                self.start_logging(self.logfile)
+            # take default termination character
+            # by default, append empty string
+            self.term_chars = kwargs.pop('term_chars', '')
+            # hand off to standard serial init function
+            super(Serial, self).__init__(*args, **kwargs)
+
+        def start_logging(self, fname):
+            """ start logging read/write data to file. """
+            # make log file handler
+            lfh = logging.FileHandler(fname)
+            self.logger.addHandler(lfh)
+            # make log file formatter
+            lff = logging.Formatter('%(asctime)s %(message)s')
+            lfh.setFormatter(lff)
+            # set level low to log everything
+            self.logger.setLevel(1)
+            self.logger.debug('opened serial port')
+
+        def write(self, data):
+            data += self.term_chars
+            super(Serial, self).write(data)
+            self.logger.debug('write: ' + show_newlines(data))
+
+        def read(self, size=1):
+            resp = super(Serial, self).read(size)
+            self.logger.debug(b' read: ' + show_newlines(resp))
+            return resp
+
+        def readall(self, term_chars=None):
+            """
+            Automatically read all the bytes from the serial port.
+
+            if :attr:`term_chars` is set, this will continue
+            to read until the terminating bytes are received.
+            This can be provided as a keyword argument.
+
+            """
+            resp = self.read(self.inWaiting())
+            if term_chars is None:
+                term_chars = self.term_chars
+            if term_chars is not '':
+                while resp[-len(term_chars):] != term_chars:
+                    resp += self.read(self.inWaiting())
+            return resp
+
+        def ask(self, query, lag=0.05):
+            """
+            Write to the bus, then read response.
+
+            This doesn't seem to work very well.
+
+            """
+            self.write(query)
+            sleep(lag)
+            return self.readall()
+
+
+# ------------------------------------------------------
+# up to this point, this file has dealt with customizing
+# communication interfaces (GPIB / RS232). What follows
+# are more random (though useful) utilities.
+# 
+# The two halves of this file serve rather disparate
+# needs, and should probably be broken in two pieces.
+# Before I actually do that I'd like to trim down 
+# dependencies in the rest of the library - I think that
+# will go a long way in reducing complexity.
+# ------------------------------------------------------
+
+from numpy import log10, floor
+
+def num(string):
+    """
+    convert string to number. decide whether to convert to int or float.
+    """
+    if '.' not in string:
+        return int(string)
+    else:
+        return float(string)
+
+def sciround(number, sigfigs=1):
+    """
+    Round a number to a desired significant figure precision.
+
+    >>> sciround(.000671438, 3)
+    .000671
+
+    """
+    exponent = floor(log10(number))
+    return round(number, -int(exponent) + (sigfigs - 1))
+
+try:
+    # importing scipy unnecessarily breaks Ctrl+C handling on windows, see:
+    # http://stackoverflow.com/questions/15457786/ctrl-c-crashes-python-after-importing-scipy-stats
+    # I am disabling this import because I never use `calibration` anymore,
+    # but having it here prevents plotgen from working properly
+    #from scipy.optimize import leastsq
+    leastsq = None
+except ImportError:
+    leastsq = None
+
+import csv
+
+if leastsq is not None:
+    class calibration(dict):
+        """
+        a class encapsulating a linear mapping based on measurements.
+
+        Stores calibration pairs, calculates fits to determine cal
+        parameters, and provides a function to do it.
+
+        """
+        def __init__(self, *args, **kw):
+            from scipy.optimize import leastsq
+            # initial guess of the cal
+            self.param = [1,0]
+            # form of the fit function
+            self.func = lambda p, x: p[0] * x + p[1]
+            dict.__init__(self, *args, **kw)
+
+        def keys(self):
+            return array(dict.keys(self))
+
+        def values(self):
+            return array(dict.values(self))
+
+        def recal(self):
+            """perform the fit routine and return parameters"""
+            diff = lambda p, x, y: y - self.func(p, x)
+            args = (self.keys(), self.values())
+            
+            result, win = leastsq(diff, self.param, args)
+            if win:
+                self.param = result
+            else:
+                print("fail!!")
+
+        def plot(self):
+            """ plot the calibration points and the fit"""
+            from pylab import plot
+            from numpy import linspace
+            plot(self.keys(), self.values(), 'o')
+            x = linspace(min(self.keys()), max(self.keys()), 100)
+            plot(self.keys(), self.values(), 'o')
+            plot(x, self.__call__(x), 'k--')
+
+
+        def __call__(self, arg):
+            """ calculate a conversion based on the current fit parameters """
+            return self.func(self.param, arg)
+
+        def save(self, filename):
+            """ 
+            save the current calibration data to csv file. pass filename as arg.
+
+            >>> cal.save('18oct.csv')
+
+            """
+            fl = open(filename, 'w')
+            wrt = csv.writer(fl)
+            for x, y in self.iteritems():
+                wrt.writerow([x, y])
+
+        def load(self, filename):
+            """
+            load a csv calibration.
+
+            >>> cal.load('18oct.csv')
+
+            """
+            rd = csv.reader(open(filename, 'r'))
+            for x, y in rd:
+                self[num(x)] = num(y)
+            self.recal()
+
+    def fit(x, y, func, guess):
+        """
+        fit data to a given functional form using least squares.
+
+        inputs:
+            x, y: data to fit, in arrays.
+            func: a function of p and x, where p is a parameter vector
+            guess: inital value of p to try.
+
+        outputs:
+            pfit: the best-fit parameter set.
+
+        """
+        diff = lambda p, x, y: y - func(p, x)
+        pfit, win = leastsq(diff, guess, (x, y))
+        if win:
+            return pfit
+        else:
+            print("fail!")
+
+def gaussian(p, x):
+    """
+    gaussian function.
+
+    p is a 4-component parameter vector defining::
+
+        0 -- a baseline offset
+        1 -- the area between curve and baseline
+        2 -- the location of the maximum
+        3 -- the standard deviation
+
+    """
+    
+    return p[0] + \
+    exp(-((x - p[2])**2)/(2 * p[3]**2)) * \
+    p[1]  /  (sqrt(2 * pi) * p[3])
+
+def monitor(function, lag = 0.3, absolute = False):
+    """
+    Periodically yield output of a function, along with timestamp.
+    Compatible with :func:`wanglib.pylab_extensions.live_plot.plotgen`.
+
+    :param function: function to call
+    :type function: function
+    :param lag: interval between calls to ``function`` (default 0.3 seconds)
+    :type lag: float
+    :param absolute: if True, yielded x values are seconds since
+                     epoch. otherwise, time since first yield.
+    :type absolute: boolean
+    :returns:   a generator object yielding t,y pairs.
+
+    """
+    start = 0 if absolute else time()
+    while True:
+        yield time() - start, function()
+        sleep(lag)
+
+def scanner(xvals, set, get, lag = 0.3):
+    """
+    Generic scan generator - useful for spectra, delay scans, whatever.
+    Compatible with :func:`wanglib.pylab_extensions.live_plot.plotgen`.
+
+    :param xvals: values of x over which to scan.
+    :type xvals: iterable
+    :param set: Function to call on each step that advances the independent
+                variable to the next value of ``xvals``. This function should
+                take that value as an argument.
+    :type set: function
+    :param get: Function to call on each step that performs the measurement.
+                The return value of this function should be the measurement
+                result.
+    :type get: function
+    :param lag: seconds to sleep between setting and measuring
+    :type lag: float
+    :returns:   a generator object yielding x,y pairs.
+
+    Example: while scanning triax wavelength, measure lockin x
+
+    >>> from triax.instruments.lockins import egg5110
+    >>> from triax.instruments.spex750m import triax320
+    >>> from wanglib.pylab_extensions import plotgen
+    >>> tr = triax320()
+    >>> li = egg5110(instrument(plx,12))
+    >>> wls = arange(770, 774, .1)
+    >>> gen = scanner(wls, set=tr.set_wl, get=li.get_x)
+    >>> result = plotgen(gen)
+
+    Sometimes we will want to set/measure an attribute of an object on each
+    step, instead of calling a method. In this case, we can provide an
+    (object, attribute_name) tuple in lieu of a function for ``set`` or ``get``.
+    For example, in place of the ``gen`` used above, we could do:
+
+    >>> gen = scanner(wls, set=(tr,'wl'), get=(li,'x'))
+
+    Avoid this if you can, though.
+    
+    """
+    for X in xvals:
+        if hasattr(set,'__call__'):
+            set(X)
+        else:
+            set[0].__setattr__(set[1], X)
+        sleep(lag)
+        if hasattr(get,'__call__'):
+            Y = get()
+        else:
+            Y = get[0].__getattribute__(get[1])
+        yield X,Y
+
+def averager(func, n, lag=0.1):
+    """
+    Given a function ``func``, returns an implementation of that
+    function that just repeats it ``n`` times, and returns an average
+    of the result.
+
+    :param func: function returning a measurement
+    :type func: function
+    :param n: number of times to call ``func``.
+    :type n: int
+    :param lag: seconds to sleep between measurements.
+    :type lag: float
+    :returns:   the average of the ``n`` measurements.
+
+    This is useful when scanning. For example, if scanning a spectrum
+    with the lockin like so:
+
+    >>> gen = scanner(wls, set=tr.set_wl, get=li.get_x)
+
+    We can implement a version that averages three lockin measurements
+    with a 0.3s delay like so:
+
+    >>> acq = averager(li.get_x, 3, lag=0.3)
+    >>> gen = scanner(wls, set=tr.set_wl, get=acq)
+
+    """
+    def f(*args, **kwargs):
+        ls = []
+        ls.append(func(*args, **kwargs))
+        for i in range(n - 1):
+            sleep(0.1)
+            ls.append(func(*args, **kwargs))
+        ar = array(ls)
+        return ar.mean(axis=0)
+    return f
+
+def save(fname, array):
+    """
+    Save a Numpy array to file.
+
+    :param fname: Filename, as a string
+    :param array: Numpy array to save.
+
+    Unlike :meth:`numpy.save`, this function will raise ValueError if
+    overwriting an existing file.
+
+    """
+    if not fname.endswith('.npy'):
+        # append file extension.
+        fname = fname + '.npy'
+        # usually the numpy function does this, but
+        # to guard against overwrites, we should do it ourselves.
+
+    try:
+        # test to see if the file exists
+        open(fname, 'r')
+    except IOError:
+        # if an error was raised, the filename is available
+        numpy.save(fname, array)
+    else:
+        # if the open statement succeeded, the filename is taken.
+        raise ValueError('file exists. choose a different name')
+
+class saver(object):
+    """
+    Sequential file saver.
+
+    after initializing :class:`saver` with the base filename, use the
+    :meth:`save` method to save arrays to sequentially-numbered files.
+
+    >>> s = saver('foo')
+    >>> s.save(arange(5)) # saves to 'foo000.npy'
+    >>> s.save(arange(2)) # saves to 'foo001.npy'
+
+    """
+
+    def __init__(self, name, verbose=False):
+        self.name = name
+        self.n = 0
+        self.verbose = verbose
+
+    def save(self, array):
+        """
+        Save an array to the next file in the sequence.
+
+        """
+        fname = "%s%03d.npy" % (self.name, self.n)
+        save(fname, array)
+        self.n +=1
+        if self.verbose:
+            print("saved as", fname)
+
+from contextlib import contextmanager
+
+@contextmanager
+def notraceback():
+    """
+    Context manager to swallow keyboard interrupt.
+
+    Execute any infinitely-looping process in this context, like:
+
+    >>> from time import sleep
+    >>> with notraceback():
+    ...     while True:
+    ...         sleep(0.1)
+
+    If you are planning to interrupt it anyway then you are
+    not interested in the traceback and this prevents your
+    output from being cluttered.
+    """
+    try:
+        yield
+    except KeyboardInterrupt:
+        pass
+
+if __name__ == "__main__":
+    from pylab import *
+    cal = calibration()
+    x = arange(40, 100, 5)
+    y = .2 * x + 5
+    y += randn(len(y))
+    param = [.2,5]
+    for i in range(len(x)):
+            cal[x[i]] = y[i]
+    cal.plot()
